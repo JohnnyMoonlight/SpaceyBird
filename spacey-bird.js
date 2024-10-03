1ca67637463e76c8457d78a3df737954e6e9014e
JohnnyMoonlight <hellmund.tobias@web.de>
2024-10-03 21:25:23 +0200

    Restructure code.

--- spacey-bird.js
+++ spacey-bird.js
@@ -1,93 +1,3 @@
-class Pipe extends Path2D {
-  constructor(
-    game,
-    pipeWidth = 50,
-    pipeHeight = 80,
-    xPosition = 300,
-    speed = 3,
-    color = "blue"
-  ) {
-    super();
-    this.game = game;
-    this.soundManager = this.game.soundManager;
-    this.soundManager.registerSoundEvent(new Event("pipePassed"));
-    this.gameHeight = this.game.gameHeight;
-    this.gameWidth = this.game.gameWidth;
-    this.speed = speed;
-    this.color = color;
-    // Number of the recently shown pipe. As soon as one pipe is passed, this value is incremented.
-    this.number = 0;
-    this.pipeWidth = pipeWidth;
-    this.pipeHeight = game.gameHeight / 2 - pipeHeight;
-    this.xPosition = xPosition;
-  }
-
-  move(ctx) {
-    this.xPosition -= this.speed;
-    if (this.xPosition < -this.pipeWidth) {
-      this.xPosition = this.gameWidth;
-      ctx.canvas.dispatchEvent(new Event("pipePassed"));
-      this.number++;
-      this._setRandomColor();
-    }
-  }
-
-  _setRandomColor() {
-    this.color = globalColorList[this.number % 140];
-  }
-
-  draw(ctx) {
-    ctx.fillStyle = this.color;
-    // Upper Pipe
-    ctx.fillRect(this.xPosition, 0, this.pipeWidth, this.pipeHeight);
-
-    // Lowe Pipe
-    ctx.fillRect(
-      this.xPosition,
-      this.gameHeight - this.pipeHeight,
-      this.pipeWidth,
-      this.pipeHeight
-    );
-  }
-
-  hitPlayer(player) {
-    const hitbox = player.getHitBox();
-    // If the tip or the back part of the triangle are within the pipes, we need to check for collision.
-    if (
-      (hitbox[0][0] > this.xPosition &&
-        hitbox[0][0] < this.xPosition + this.pipeWidth) ||
-      (hitbox[1][0] > this.xPosition &&
-        hitbox[1][0] < this.xPosition + this.pipeWidth)
-    ) {
-      let upperLineY1 = hitbox[0][1];
-      let upperLineY2 = hitbox[1][1];
-      // The hitbox is formed as triangle.
-      // The player is hit, if the upper or lower line of the triangle hit the pipe.
-      // The pipe is hit, if any integer y-pixel has a value in the range of the pipe-heights.
-      for (
-        let pixelOnUpperLine = upperLineY1;
-        pixelOnUpperLine < upperLineY2;
-        pixelOnUpperLine++
-      ) {
-        if (pixelOnUpperLine < this.pipeHeight) return true;
-        if (pixelOnUpperLine > this.gameHeight - this.pipeHeight) return true;
-      }
-      let lowerLineY1 = hitbox[2][1];
-      let lowerLineY2 = hitbox[1][1];
-
-      for (
-        let pixelOnLowerLine = lowerLineY1;
-        pixelOnLowerLine < lowerLineY2;
-        pixelOnLowerLine++
-      ) {
-        if (pixelOnLowerLine < this.pipeHeight) return true;
-        if (pixelOnLowerLine > this.gameHeight - this.pipeHeight) return true;
-      }
-      return false;
-    }
-  }
-}
-
 class Game {
   GAME_STATES = {
     MENU: 1,
@@ -251,6 +161,199 @@ class Game {
   }
 }
 
+
+class PlayerImageSelector {
+  imageMap = new Map();
+  constructor() {
+    this.imageFullPower = new Image();
+    this.imageFullPower.src = "./resources/rocket_blue_full_power.png";
+    this.imageHalfPower = new Image();
+    this.imageHalfPower.src = "./resources/rocket_blue_half_power.png";
+    this.imageNoPower = new Image();
+    this.imageNoPower.src = "./resources/rocket_blue_no_power.png";
+
+    this.imageMap.set("fullPower", this.imageFullPower);
+    this.imageMap.set("halfPower", this.imageHalfPower);
+    this.imageMap.set("noPower", this.imageNoPower);
+  }
+
+  getImageForPlayer(player) {
+    const acc = player.verticalAcceleration;
+    if (acc < -3) return this.imageMap.get("fullPower");
+    else if (acc >= -3 && acc < 0) return this.imageMap.get("halfPower");
+    else if (acc > 0) return this.imageMap.get("noPower");
+    return this.imageMap.get("noPower");
+  }
+}
+
+class Player extends Path2D {
+  constructor(canvas, game, playerImageSelector, playerX = 20, playerY = 10) {
+    super();
+    this.playerImage = playerImageSelector;
+    this.game = game;
+    this.playerX = playerX;
+    this.playerY = playerY;
+    this.verticalAcceleration = -1;
+    this.flapPower = -8;
+    this.gravity = 0.2;
+    this.canvas = canvas;
+    this.soundManager = game.soundManager;
+    this.flapEvent = new Event("flap");
+    this.image = new Image();
+    this.image.src = "./resources/rocket_blue_full_power.png";
+    this.soundManager.registerSoundEvent(this.flapEvent);
+    this.canvas.addEventListener("keyup", this._flapHandle);
+  }
+
+  _flapHandle = (event) => {
+    if (event.code == "Space") {
+      this.flap();
+    }
+  };
+
+  draw(ctx) {
+    ctx.drawImage(
+      this.playerImage.getImageForPlayer(this),
+      this.playerX,
+      this.playerY,
+      60,
+      30
+    );
+    if (this.game.debug) {
+      ctx.save();
+      ctx.lineWidth = 1;
+      ctx.strokeStyle = "red";
+      const poly = this.getHitBox();
+      ctx.beginPath();
+      ctx.moveTo(poly[0][0], poly[0][1]);
+      for (let i = 1; i < poly.length; i++) {
+        ctx.lineTo(poly[i][0], poly[i][1]);
+      }
+      ctx.closePath();
+      ctx.stroke();
+      ctx.restore();
+    }
+  }
+
+  /**
+   * Returns array of x,y coordinate pairs.
+   * The first coordinate is top-left, the second the nose/tip of the player, the third is the bottom-left coordinate.
+   */
+  getHitBox() {
+    let hitbox = [
+      [this.playerX, this.playerY],
+      [this.playerX + 60, this.playerY + 15],
+      [this.playerX, this.playerY + 30],
+    ];
+    return hitbox;
+  }
+
+  move() {
+    if (this.playerY < this.game.gameHeight && this.playerY >= 0) {
+      this.verticalAcceleration += this.gravity;
+    } else if (this.playerY < 0) {
+      this.verticalAcceleration = 0;
+      this.playerY = 0;
+    }
+    this.playerY = this.playerY + this.verticalAcceleration;
+  }
+
+  flap() {
+    this.verticalAcceleration = this.flapPower;
+    this.canvas.dispatchEvent(this.flapEvent);
+  }
+}
+
+class Pipe extends Path2D {
+  constructor(
+    game,
+    pipeWidth = 50,
+    pipeHeight = 80,
+    xPosition = 300,
+    speed = 3,
+    color = "blue"
+  ) {
+    super();
+    this.game = game;
+    this.soundManager = this.game.soundManager;
+    this.soundManager.registerSoundEvent(new Event("pipePassed"));
+    this.gameHeight = this.game.gameHeight;
+    this.gameWidth = this.game.gameWidth;
+    this.speed = speed;
+    this.color = color;
+    // Number of the recently shown pipe. As soon as one pipe is passed, this value is incremented.
+    this.number = 0;
+    this.pipeWidth = pipeWidth;
+    this.pipeHeight = game.gameHeight / 2 - pipeHeight;
+    this.xPosition = xPosition;
+  }
+
+  move(ctx) {
+    this.xPosition -= this.speed;
+    if (this.xPosition < -this.pipeWidth) {
+      this.xPosition = this.gameWidth;
+      ctx.canvas.dispatchEvent(new Event("pipePassed"));
+      this.number++;
+      this._setRandomColor();
+    }
+  }
+
+  _setRandomColor() {
+    this.color = globalColorList[this.number % 140];
+  }
+
+  draw(ctx) {
+    ctx.fillStyle = this.color;
+    // Upper Pipe
+    ctx.fillRect(this.xPosition, 0, this.pipeWidth, this.pipeHeight);
+
+    // Lowe Pipe
+    ctx.fillRect(
+      this.xPosition,
+      this.gameHeight - this.pipeHeight,
+      this.pipeWidth,
+      this.pipeHeight
+    );
+  }
+
+  hitPlayer(player) {
+    const hitbox = player.getHitBox();
+    // If the tip or the back part of the triangle are within the pipes, we need to check for collision.
+    if (
+      (hitbox[0][0] > this.xPosition &&
+        hitbox[0][0] < this.xPosition + this.pipeWidth) ||
+      (hitbox[1][0] > this.xPosition &&
+        hitbox[1][0] < this.xPosition + this.pipeWidth)
+    ) {
+      let upperLineY1 = hitbox[0][1];
+      let upperLineY2 = hitbox[1][1];
+      // The hitbox is formed as triangle.
+      // The player is hit, if the upper or lower line of the triangle hit the pipe.
+      // The pipe is hit, if any integer y-pixel has a value in the range of the pipe-heights.
+      for (
+        let pixelOnUpperLine = upperLineY1;
+        pixelOnUpperLine < upperLineY2;
+        pixelOnUpperLine++
+      ) {
+        if (pixelOnUpperLine < this.pipeHeight) return true;
+        if (pixelOnUpperLine > this.gameHeight - this.pipeHeight) return true;
+      }
+      let lowerLineY1 = hitbox[2][1];
+      let lowerLineY2 = hitbox[1][1];
+
+      for (
+        let pixelOnLowerLine = lowerLineY1;
+        pixelOnLowerLine < lowerLineY2;
+        pixelOnLowerLine++
+      ) {
+        if (pixelOnLowerLine < this.pipeHeight) return true;
+        if (pixelOnLowerLine > this.gameHeight - this.pipeHeight) return true;
+      }
+      return false;
+    }
+  }
+}
+
 class MenuEntry {
   constructor(title, callback, color = "#000000") {
     this.title = title;
@@ -298,6 +401,7 @@ class Menu extends Path2D {
     }
     ctx.stroke();
   }
+
   handleMenuInput(event, canvas) {
     const key = event.key;
     if (key == "ArrowUp" || key == "ArrowDown" || key == "Enter") {
@@ -320,110 +424,15 @@ class Menu extends Path2D {
       }
     }
   }
+
   _handleMenuInput = (event, canvas) => this.handleMenuInput(event, canvas);
 
   initListeners(canvas) {
     canvas.addEventListener("keyup", this._handleMenuInput);
   }
+
   _releaseListeners(canvas) {
     canvas.removeEventListener("keyup", this._handleMenuInput);
-  }
-}
-
-class PlayerImageSelector {
-  imageMap = new Map();
-  constructor() {
-    this.imageFullPower = new Image();
-    this.imageFullPower.src = "./resources/rocket_blue_full_power.png";
-    this.imageHalfPower = new Image();
-    this.imageHalfPower.src = "./resources/rocket_blue_half_power.png";
-    this.imageNoPower = new Image();
-    this.imageNoPower.src = "./resources/rocket_blue_no_power.png";
-
-    this.imageMap.set("fullPower", this.imageFullPower);
-    this.imageMap.set("halfPower", this.imageHalfPower);
-    this.imageMap.set("noPower", this.imageNoPower);
-  }
-
-  getImageForPlayer(player) {
-    const acc = player.verticalAcceleration;
-    if (acc < -3) return this.imageMap.get("fullPower");
-    else if (acc >= -3 && acc < 0) return this.imageMap.get("halfPower");
-    else if (acc > 0) return this.imageMap.get("noPower");
-    return this.imageMap.get("noPower");
-  }
-}
-
-class Player extends Path2D {
-  constructor(canvas, game, playerImageSelector, playerX = 20, playerY = 10) {
-    super();
-    this.playerImage = playerImageSelector;
-    this.game = game;
-    this.playerX = playerX;
-    this.playerY = playerY;
-    this.verticalAcceleration = -1;
-    this.flapPower = -8;
-    this.gravity = 0.2;
-    this.canvas = canvas;
-    this.soundManager = game.soundManager;
-    this.flapEvent = new Event("flap");
-    this.image = new Image();
-    this.image.src = "./resources/rocket_blue_full_power.png";
-    this.soundManager.registerSoundEvent(this.flapEvent);
-    this.canvas.addEventListener("keyup", this._flapHandle);
-  }
-  _flapHandle = (event) => {
-    if (event.code == "Space") {
-      this.flap();
-    }
-  };
-  draw(ctx) {
-    ctx.drawImage(
-      this.playerImage.getImageForPlayer(this),
-      this.playerX,
-      this.playerY,
-      60,
-      30
-    );
-    if (this.game.debug) {
-      ctx.save();
-      ctx.lineWidth = 1;
-      ctx.strokeStyle = "red";
-      const poly = this.getHitBox();
-      ctx.beginPath();
-      ctx.moveTo(poly[0][0], poly[0][1]);
-      for (let i = 1; i < poly.length; i++) {
-        ctx.lineTo(poly[i][0], poly[i][1]);
-      }
-      ctx.closePath();
-      ctx.stroke();
-      ctx.restore();
-    }
-  }
-  /**
-   * Returns array of x,y coordinate pairs.
-   * The first coordinate is top-left, the second the nose/tip of the player, the third is the bottom-left coordinate.
-   */
-  getHitBox() {
-    let hitbox = [
-      [this.playerX, this.playerY],
-      [this.playerX + 60, this.playerY + 15],
-      [this.playerX, this.playerY + 30],
-    ];
-    return hitbox;
-  }
-  move() {
-    if (this.playerY < this.game.gameHeight && this.playerY >= 0) {
-      this.verticalAcceleration += this.gravity;
-    } else if (this.playerY < 0) {
-      this.verticalAcceleration = 0;
-      this.playerY = 0;
-    }
-    this.playerY = this.playerY + this.verticalAcceleration;
-  }
-  flap() {
-    this.verticalAcceleration = this.flapPower;
-    this.canvas.dispatchEvent(this.flapEvent);
   }
 }
 
